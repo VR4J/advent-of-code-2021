1e1da30d03819f2e1f150405ff24886d393cebe9
Joery Vreijsen <joery.vreijsen@engie.com>
2021-12-01 10:32:43 +0100

    Implement solution day one; part two

--- Day-1.kt
+++ Day-1.kt
@@ -15,10 +15,10 @@ fun runPartOne(input: List<String>) {
     for (i in input.indices) {
         if(i == 0) continue;
 
-        val currentLine = input[i].toInt();
-        val previousLine = input[i - 1].toInt();
+        val current = input[i].toInt();
+        val previous = input[i - 1].toInt();
 
-        if(currentLine > previousLine) {
+        if(current > previous) {
             count++;
         }
     }
@@ -27,5 +27,41 @@ fun runPartOne(input: List<String>) {
 }
 
 fun runPartTwo(input: List<String>) {
-    // TODO
+    val windows = getMeasurementWindows(input);
+    var count = 0;
+
+    for (i in windows.indices) {
+        if(i == 0) continue;
+
+        val (cFirst, cSecond, cThird) = windows[i]
+        val (pFirst, pSecond, pThird) = windows[i - 1]
+
+        val cTotal = cFirst + cSecond + cThird;
+        val pTotal = pFirst + pSecond + pThird;
+
+        if(cTotal > pTotal) {
+            count++;
+        }
+    }
+
+    println("Window sums increased $count times.")
+}
+
+fun getMeasurementWindows(input: List<String>): List<Triple<Int, Int, Int>> {
+    val windows = ArrayList<Triple<Int, Int, Int>>()
+
+    for (i in input.indices) {
+        // Break if not enough measurements left to create a window.
+        if((i + 2) > (input.size - 1)) break;
+
+        val first = input[i].toInt()
+        val second = input[i + 1].toInt()
+        val third = input[i + 2].toInt()
+
+        windows.add(
+            Triple(first, second, third)
+        )
+    }
+
+    return windows;
 }
