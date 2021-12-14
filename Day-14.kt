2536be19c39ce40529701a1e0191a97c8c9b28d0
Joery Vreijsen <joery.vreijsen@engie.com>
2021-12-14 23:02:44 +0100

    Update Day-14.kt

--- Day-14.kt
+++ Day-14.kt
@@ -29,7 +29,7 @@ fun polymerization(template: String, rules: List<Rule>, steps: Int) {
         for((pair, value) in HashMap(pairs)) {
             pairs.decrease(pair, value)
 
-            val rule = rules.find { it.elements == pair }
+            val rule = rules.find { it.elements == pair } !!
 
             pairs.increase(Pair(pair.first, rule.insertion), value)
             pairs.increase(Pair(rule.insertion, pair.second), value)
